5082938a6c350721925b8cc80206a49345bc4abb
TekLoop <kipr0596@yandex.ru>
2022-11-03 23:37:06 +0300

    if\else

---
+++ ex005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("insert your name");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "ваня") // tolower перевод все буквы в нижний регист и без разницы как написать "маша"
+{
+    Console.WriteLine("Ура, это же ВАНЕК");
+}
+else
+{
+    Console.Write("FuckYou, ");
+    Console.Write (username);
+}
