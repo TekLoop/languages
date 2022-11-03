210421e4e33d605a96fda2e089db2907bd9a9778
TekLoop <kipr0596@yandex.ru>
2022-11-04 00:07:45 +0300

    base1

--- ex006_base/Program.cs
+++ ex006_base/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = 1;
+int b = 3;
+int c = 6;
+int d = 9;
+int e = 2;
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+if (d > max) max = d;
+if (e > max) max = e;
+
+Console.Write ("max = ");
+Console.Write (max);
