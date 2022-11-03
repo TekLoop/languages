47d93b1883eadcfb96fa4928baa8c24fceeb6010
TekLoop <kipr0596@yandex.ru>
2022-11-03 17:34:38 +0300

    hello console

---
+++ ex001_hello_console/Program.cs
@@ -0,0 +1,5 @@
+﻿// начинаем знакомство, все что за двойным слэшем это просто комментарий на код никак не влияет
+//  чтобы создать консоль введи dotnet new console
+// чтобы заупустить написанную прогу donten run
+Console.WriteLine("2 Hello, World!");
+Console.WriteLine("2 Hello, World!");
