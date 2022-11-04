ba5414f446389fef90df75b5f087627cfa3fc3ba
TekLoop <kipr0596@yandex.ru>
2022-11-04 17:49:11 +0300

    compare

---
+++ ex009_compare/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+Console.WriteLine("введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите второе число");
+int y = Convert.ToInt32(Console.ReadLine());
+int c;
+
+if (x > y) // можно через оператор или ||(if (x*x=y || y*y=x)); также есть оператор и &&
+{
+    c = x / y;
+        if (c == y)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+}
+else
+{
+    c = y /x;
+    if (c == x)
+    {
+         Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
