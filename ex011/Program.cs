bdd8a03b05550da3f78804b2bcade2ca9202f69c
TekLoop <kipr0596@yandex.ru>
2022-11-05 09:58:31 +0300

    ex011

---
+++ ex011/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("int first number");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("int second number");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("int third number");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a == b && b == c)
+Console.WriteLine("numbers are the same");
+
+    if (a > b && a > c)
+    {
+        Console.WriteLine($"max = {a}");
+    }
+    else
+    {
+        if (b > a && b > c)
+        {
+            Console.WriteLine($"max = {b}");
+        }
+        else
+        {
+            Console.WriteLine($"max = {c}");
+        }        
+    }
